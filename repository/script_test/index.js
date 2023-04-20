1ab4b811c8cc5bb2d1ff6827762bc23bc3146766
Zyao89 <zyao89@gmail.com>
2023-04-20 20:28:35 +0800

    feat: update script_test

--- repository/script_test/index.js
+++ repository/script_test/index.js
@@ -6,12 +6,15 @@ console.info('test name:', params.name);
 const cheerio = require('cheerio');
 
 const request = require('request');
-request('https://www.baidu.com', function (error, response, body) {
-    if (error) {
-        console.error(error);
-    }
-    if (!error && response.statusCode == 200) {
-        const $ = cheerio.load(body);
-        console.log('title:', $('title').text()); // Show the HTML for the Google homepage.
-    }
+
+module.exports = new Promise(resolve => {
+    request('https://www.baidu.com', function (error, response, body) {
+        if (error) {
+            console.error(error);
+        } else if (response.statusCode == 200) {
+            const $ = cheerio.load(body);
+            console.log('title:', $('title').text()); // Show the HTML for the Google homepage.
+        }
+        resolve(body);
+    });
 });
